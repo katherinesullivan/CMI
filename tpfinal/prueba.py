8afcd5b31b82498fe9b896adaf688243ef616637
inescipullo <64146923+inescipullo@users.noreply.github.com>
2020-12-03 16:27:10 -0300

    final

---
+++ tpfinal/prueba.py
@@ -0,0 +1,36 @@
+import argparse
+import matplotlib.pyplot as plt
+import numpy as np
+
+def coordenadas_random(N):
+    return np.random.rand(N) * 1000 
+
+
+def draws_graph(grafo):
+    n_vertices = len(grafo[0])
+    x_coordenadas = coordenadas_random(n_vertices)
+    y_coordenadas = coordenadas_random(n_vertices)
+    plt.scatter(x_coordenadas,y_coordenadas)
+    for i in range(n_vertices):
+        x_aristas = []
+        y_aristas = []
+        for j in range(n_vertices):
+            v1 = grafo[0][i]
+            v2 = grafo[0][j]
+            if (v1,v2) in grafo[1]:
+                x_aristas.append(x_coordenadas[i])
+                x_aristas.append(x_coordenadas[j])
+                y_aristas.append(y_coordenadas[i])
+                y_aristas.append(y_coordenadas[j])
+                plt.plot(x_aristas,y_aristas)
+    plt.draw()
+    plt.show()
+    
+
+
+def main():
+    g = (['A','B','C'],[('A','B'),('B','C'),('C','A')])
+    g=draws_graph(g)
+
+if __name__ == '__main__':
+    main()
